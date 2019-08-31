133fb7c3b0493b147b9fe5d3a723aeffb22bb762
Matt Wong <mattwong59@gmail.com>
2019-08-30 22:07:03 -0700

    Add active & hover styles to header

--- src/Nav.js
+++ src/Nav.js
@@ -4,48 +4,56 @@ export default class Nav extends Component {
   render() {
     return (
       <header>
-        <ul>
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Work</a>
-          </li>
-          <li>
-            <a href="">Contact</a>
-          </li>
-          <li>
-            <a
-              href="mailto:mattwong59@gmail.com"
-              target="_blank"
-              class="far fa-envelope"
-            />
-          </li>
-          <li>
-            <a
-              href="https://github.com/mattwong59"
-              target="_blank"
-              class="fab fa-github"
-            />
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/mattwong59/"
-              target="_blank"
-              class="fab fa-linkedin"
-            />
-          </li>
-          <li>
-            <a
-              href="https://codepen.io/mattwong59"
-              target="_blank"
-              class="fab fa-codepen"
-            />
-          </li>
-        </ul>
+        <div className="logo">MW</div>
+        <nav>
+          <ul>
+            <li>
+              <a href="" className="active">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Work</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+            <li>
+              <a
+                href="mailto:mattwong59@gmail.com"
+                target="_blank"
+                className="far fa-envelope"
+              />
+            </li>
+            <li>
+              <a
+                href="https://github.com/mattwong59"
+                target="_blank"
+                className="fab fa-github"
+              />
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/mattwong59/"
+                target="_blank"
+                className="fab fa-linkedin"
+              />
+            </li>
+            <li>
+              <a
+                href="https://codepen.io/mattwong59"
+                target="_blank"
+                className="fab fa-codepen"
+              />
+            </li>
+          </ul>
+        </nav>
+        <div className="menu-toggle">
+          <i className="fas fa-bars" aria-hidden="true"></i>
+        </div>
       </header>
     );
   }
